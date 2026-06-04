cc799f4acb02f8c326973cae7424825f3195621d
Muzammil Hussain <muzammilch2021@gmail.com>
2026-06-04 15:45:32 +0500

    Adding About US, Contact US, and Place Order Pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,51 @@
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+import NewsletterBox from "../components/NewsletterBox";
 
 
 const About = () => {
   return (
     <div>
-      <h2>About</h2>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'ABOUT'} text2={"US"}/>
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img className="w-full md:max-w-[450px]" src={assets.about_img}/>
+        <div className="flex flex-col justify-center gap-6 md:w-2/4">
+         <p>Shopix is a leading online retailer offering a wide range of products at competitive prices. Our mission is to provide an exceptional shopping experience with fast delivery and outstanding customer service.</p>
+         <p>Since our founding, we have been committed to delivering quality products and exceptional service to our customers.</p>
+         <b className='text-gray-800'>Our Mission</b>
+         <p>Our mission is to be the most trusted and beloved shopping destination for our customers.</p>
+        </div>
+
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'Why'} text2={"CHOOSE US"}/>
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className='border px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance</b>
+          <p className='text-gray-600'>All our products undergo rigorous testing to ensure they meet the highest quality standards.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience</b>
+          <p className='text-gray-600'>With our user-friendly interface and seamless shopping experience, we make it easy for you to find and purchase the products you love.</p>
+        </div>
+
+
+        <div className='border px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service</b>
+          <p className='text-gray-600'>Our dedicated customer service team is always ready to assist you with any questions or concerns you may have.</p>
+        </div>
+
+
+
+      </div>
+
+      <NewsletterBox/>
+
     </div>
   );
 };
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,9 +2,10 @@ import { useContext, useState, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -57,11 +58,28 @@ const Cart = () => {
                   </div>
                 </div> 
               </div>
-              <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
-              <img src={assets.bin_icon} className="w-4 cursor-pointer mr-4 sm:w-5" alt="" />
+              <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} onChange={(e)=>updateQuantity(item._id, item.size, parseInt(e.target.value))} />
+              <img onClick={()=>updateQuantity(item._id, item.size, 0)} src={assets.bin_icon} className="w-4 cursor-pointer mr-4 sm:w-5" alt="" />
             </div>
           );
         })}
+      </div>
+
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal/>
+
+          <div className='w-full text-end'>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white px-8 py-3 text-sm'>PROCEED TO CHECKOUT</button>
+          </div>
+
+
+
+
+
+
+
+        </div>
       </div>
     </div>
   );
